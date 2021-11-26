907a8ff3e575b4de2afaf3cbb36ba885c9fd602d
joedietrich-dev <joe@joedietrich.net>
2021-11-26 17:55:53 -0500

    commit tree 1f6f8e12e5832a611fb51b44bea015061d499b11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,21 @@ class ApplicationController < Sinatra::Base
     bakery.to_json(include: :baked_goods)
   end
 
+  patch '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.update(name: params[:name])
+    bakery.to_json
+  end
+
+  get '/baked_goods' do
+    BakedGood.all.to_json
+  end
+
+  post '/baked_goods' do
+    baked_good = BakedGood.create(name: params[:name], price: params[:price], bakery_id: params[:bakery_id])
+    baked_good.to_json
+  end
+
   get '/baked_goods/by_price' do
     # see the BakedGood class for the  method definition of `.by_price`
     baked_goods = BakedGood.by_price
@@ -20,6 +35,17 @@ class ApplicationController < Sinatra::Base
   get '/baked_goods/most_expensive' do
     # see the BakedGood class for the  method definition of `.by_price`
     baked_good = BakedGood.by_price.first
+    baked_good.to_json
+  end
+  
+  get '/baked_goods/:id' do
+    baked_good = BakedGood.find(params[:id])
+    baked_good.to_json
+  end
+
+  delete '/baked_goods/:id' do
+    baked_good = BakedGood.find(params[:id])
+    baked_good.destroy
     baked_good.to_json
   end
 
